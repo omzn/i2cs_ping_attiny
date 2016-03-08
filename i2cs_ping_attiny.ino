dffe7f4e53385262d56b180a8909a553242031dd
Osamu Mizuno <o-mizuno@kit.ac.jp>
2016-03-08 22:12:15 +0900

    Changes to be committed: 	modified:   i2cs_ping_attiny.ino Using EEPROM write with TinyWireS will fail. Not to use EEPROM while i2c communication.

--- i2cs_ping_attiny.ino
+++ i2cs_ping_attiny.ino
@@ -5,69 +5,78 @@
 
 #include <TinyWireS.h>              // Requires fork by Rambo with onRequest support
 #include <TinyNewPing.h>            // NewPing library modified for ATtiny
-#include <EEPROM.h>
+//#include <EEPROM.h>
 
-#define LED_R_PIN 1
-#define LED_G_PIN 4
-#define TRIG_PIN 3
-#define ECHO_PIN 3
+#define LED_R_PIN (1)
+#define LED_G_PIN (4)
+#define TRIG_PIN (3)
+#define ECHO_PIN (3)
 
-#define EEPROM_THRES_ADDR 0
+//#define EEPROM_THRES_ADDR 0
 
-const byte SensorTrigPin = 3;        // Sensor 1 is connected to PB3
-const byte SensorEchoPin = 3;        // Sensor 1 is connected to PB3
-const int I2CSlaveAddress = 0x26;      // I2C Address.
-
-byte distance;                              // Where the Distance is stored (8 bit unsigned)
-uint8_t thres[2]; // 0-warn, 1-emerg
+#define SLAVE_ADDR (0x26)
 
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-NewPing SensorOne (SensorTrigPin, SensorEchoPin, MAX_DISTANCE);       // Define the Sensor
 
+byte distance;                              // Where the Distance is stored (8 bit unsigned)
+uint8_t thres[2]; // 0-warn, 1-emerg
+unsigned long ptime = 0;
+uint8_t ledstatus = 0;
+NewPing SensorOne (TRIG_PIN, ECHO_PIN, MAX_DISTANCE);       // Define the Sensor
+bool thres_changed;
 
 void setup()
 {
   delay(100);
-  EEPROM.begin();
+//  EEPROM.begin();
 
   pinMode(LED_R_PIN, OUTPUT);
   pinMode(LED_G_PIN, OUTPUT);
   digitalWrite(LED_R_PIN, LOW);
   digitalWrite(LED_G_PIN, LOW);
 
-  thres[0] = EEPROM.read(EEPROM_THRES_ADDR + 0);
-  thres[1] = EEPROM.read(EEPROM_THRES_ADDR + 1);
-  if (thres[0] == 255) {
-    thres[0] = 0;
-  }
-  if (thres[1] == 255) {
-    thres[1] = 0;
-  }
+//  thres[0] = EEPROM.read(EEPROM_THRES_ADDR + 0);
+//  thres[1] = EEPROM.read(EEPROM_THRES_ADDR + 1);
 
-  TinyWireS.begin(I2CSlaveAddress);      // Begin I2C Communication
+//  if (thres[0] == 255) {
+    thres[0] = 0;
+//  }
+//  if (thres[1] == 255) {
+    thres[1] = 0;
+//  }
+
+  TinyWireS.begin(SLAVE_ADDR);      // Begin I2C Communication
   TinyWireS.onRequest(transmit);         // When requested, call function transmit()
   TinyWireS.onReceive(set_thres);
 }
-
 
 void loop()
 {
   //distance = SensorOne.ping_cm();        // Get distance in cm. Could be changed to
   distance = SensorOne.ping_median(10) / US_ROUNDTRIP_CM;     // Take the median of 5 readings
-
+//  if (thres_changed) {
+//    EEPROM.update(EEPROM_THRES_ADDR + 0, thres[0]);
+//    EEPROM.update(EEPROM_THRES_ADDR + 1, thres[1]);
+//    thres_changed = false;
+//  }
   delay(30);                             // Delay to avoid interference from last ping
 
+  unsigned long t = millis();
   if (thres[0] > 0 && thres[1] > 0) {
     if (distance >= thres[0] && distance < thres[1]) {
-      digitalWrite(LED_R_PIN,HIGH);
-      digitalWrite(LED_G_PIN,HIGH);
+      digitalWrite(LED_R_PIN, HIGH);
+      digitalWrite(LED_G_PIN, HIGH);
     } else if (distance >= thres[0] && distance >= thres[1]) {
-      digitalWrite(LED_R_PIN,HIGH);
-      digitalWrite(LED_G_PIN,LOW);      
+      if (t - ptime > 1000) {
+        ledstatus = 1 - ledstatus;
+        digitalWrite(LED_R_PIN, ledstatus);
+        digitalWrite(LED_G_PIN, LOW);
+        ptime = t;
+      }
     } else {
-      digitalWrite(LED_R_PIN,LOW);
-      digitalWrite(LED_G_PIN,HIGH);            
+      digitalWrite(LED_R_PIN, LOW);
+      digitalWrite(LED_G_PIN, HIGH);
     }
   }
 }
@@ -79,15 +88,13 @@ void set_thres(uint8_t y) {
     return;
   }
   while (y--) {
-//  while (TinyWireS.available()) {
     x[i] = TinyWireS.receive(); // receive byte as a character
     i++;
   }
   if (i > 1) {
-    thres[0] = x[0];    
-    thres[1] = x[1];    
-    EEPROM.update(EEPROM_THRES_ADDR + 0,thres[0]);
-    EEPROM.update(EEPROM_THRES_ADDR + 1,thres[1]);
+    thres[0] = x[0];
+    thres[1] = x[1];
+//    thres_changed = true;
   }
 }
 
